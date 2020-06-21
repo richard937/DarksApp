52415dd4722370ed57f40c0f80c196c4d543a70f
richard937 <papuncharan@gmail.com>
2020-06-21 21:03:06 +0530

    added all the files

---
+++ script.js
@@ -0,0 +1,10 @@
+//chrome.tabs.executeScript(tab.id, { code: "console.log('tab found');" }, function () {
+// if (chrome.runtime.lastError) {
+//     console.log("ERROR: " + chrome.runtime.lastError.message);
+// }
+// });
+// let msg = "Tab found";
+// chrome.tabs.executeScript(tab.id, {
+//     //file: 'script.js'
+//     code: "console.log(msg);"
+// });
